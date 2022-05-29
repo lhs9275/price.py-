7ef9ff12d63997fc3f34bbedaf9c589a82b7d0d6
lhs9275 <98660600+lhs9275@users.noreply.github.com>
2022-05-30 04:16:05 +0900

    Update price.py

--- price.py
+++ price.py
@@ -1,4 +1,15 @@
 def shopping(shop_file):
+    base_url = "https://raw.githubusercontent.com/codingalzi/pybook/master/jupyter-book/data/"
+    def myWget(filename):
+    # 다운로드 대상 파일 경로
+        file_url = base_url + filename
+
+    # 저장 경로와 파일명
+        target_path = data_path / filename
+
+        return urlretrieve(file_url, target_path)
+
+    myWget("shopA.txt")
     k = "./data/"+shop_file+""
     
     shop_dict = {} # 생성할 사전 객체   
@@ -17,7 +28,18 @@ def shopping(shop_file):
 
 
 
-def item_price(shop_file, item):
+def item_price(shop_file, item): 
+    base_url = "https://raw.githubusercontent.com/codingalzi/pybook/master/jupyter-book/data/"
+    def myWget(filename):
+    # 다운로드 대상 파일 경로
+        file_url = base_url + filename
+
+    # 저장 경로와 파일명
+        target_path = data_path / filename
+
+        return urlretrieve(file_url, target_path)
+
+    myWget("shopA.txt")
     k = "./data/"+shop_file+""
 
     shop_dict = {} # 생성할 사전 객체   
